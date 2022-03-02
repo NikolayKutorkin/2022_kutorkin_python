64380fa8b84600c2eb3e6f8a50aee5dde67a04b6
Kutorkin Nikolay <kutorkin.ne@phystech.edu>
2022-03-02 08:41:06 +0300

    Не идеальная версия, но крутой след от шаров

--- lab6/Catch_ball.py
+++ lab6/Catch_ball.py
@@ -5,7 +5,7 @@ from numpy import sin, cos, pi
 
 pygame.init()
 
-FPS = 10
+FPS = 30
 screen = pygame.display.set_mode((1200, 700))
 
 RED = (255, 0, 0)
@@ -16,40 +16,49 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+l = 4
 
 
 def new_ball():
     '''рисует новый шарик '''
+    screen.fill(BLACK)
     x = randint(100, 1100)
-    y = randint(100, 900)
-    r = randint(10, 100)
+    y = randint(100, 650)
+    r = randint(30, 100)
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
-    return x, y, r, color
+    pygame.display.update()
+    angle = randint(0, 360)
+    return [x, y, r, color, angle]
 
 
-def move_ball(x, y, r, color, vector):
-    x = x + vector[0]
-    y = y + vector[1]
-    screen.fill(BLACK)
+def move_ball(ball):
+    x = ball[0]
+    y = ball[1]
+    r = ball[2]
+    color = ball[3]
+    angle = ball[4]
+    circle(screen, (0, 0, 0), (x, y), r + 2)
+    ball[0] += l * cos(ball[4] * pi / 180)
+    ball[1] += l * sin(ball[4] * pi / 180)
     circle(screen, color, (x, y), r)
     pygame.display.update()
-    return x, y, r, color
+    return [x, y, r, color, angle]
 
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
-# xb = -1
-# yb = -1
-# r = 0
-score = 0
 
-xb, yb, r, color = new_ball()
-l = 4
-agle = randint(0, 360)
-direction = [l * cos(agle * pi / 180), l * sin(agle * pi / 180)]
+score = 0
+box = []
+# 0 , 1 , 2,     3,     4.
+# xb, yb, r, color, angle = new_ball()
+# xb, yb, r, color, angle = new_ball()
+for i in range(7):
+    ball = new_ball()
+    box.append(ball)
 pygame.display.update()
 while not finished:
     clock.tick(FPS)
@@ -58,33 +67,39 @@ while not finished:
             print('Score: ', score)
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = event.pos
-            if (x - xb) ** 2 + (y - yb) ** 2 <= (r ** 2):
-                print('Nice')
-                score += 100
-                screen.fill(BLACK)
-                xb, yb, r, color = new_ball()
-                l = 0
-                agle = randint(0, 360)
-                direction = [l * cos(agle * pi / 180), l * sin(agle * pi / 180)]
-                pygame.display.update()
+            for ball in box:
+                x, y = event.pos
+                if (x - ball[0]) ** 2 + (y - ball[1]) ** 2 <= ball[2] ** 2:
+                    print('Nice')
+                    score += 100
+                    xb, yb, r, color, angle = new_ball()
+                    ball[0] = xb
+                    ball[1] = yb
+                    ball[2] = r
+                    ball[3] = color
+                    ball[4] = angle
 
+                else:
+                    print('Ha')
+    # screen.fill(BLACK)
+    for ball in box:
+        if ball[0] < ball[2] + 2 or ball[0] > 1200 - ball[2] - 2:
+            if 270 > ball[4] > 90:
+                ball[4] = randint(-50, 50)
             else:
-                print('Ha')
-    xb, yb, r, color = move_ball(xb, yb, r, color, direction)
-    # direction = [l * cos(agle * pi / 180), l * sin(agle * pi / 180)]
-    if xb < 20 or xb > 1180:
-        if agle < 270 and agle > 90:
-            agle = randint(-90, 90)
-        else:
-            agle = randint(90, 270)
-        direction = [l * cos(agle * pi / 180), l * sin(agle * pi / 180)]
-        xb, yb, r, color = move_ball(xb, yb, r, color, direction)
-    if yb < 20 or yb > 680:
-        if agle > 0 and agle < 180:
-            agle = randint(180, 360)
-        else:
-            agle = randint(0, 180)
-        direction = [l * cos(agle * pi / 180), l * sin(agle * pi / 180)]
-        xb, yb, r, color = move_ball(xb, yb, r, color, direction)
+                ball[4] = randint(130, 230)
+            ball = move_ball(ball)
+            ball = move_ball(ball)
+            ball = move_ball(ball)
+
+        if ball[1] < ball[2] + 2 or ball[1] > 700 - ball[2] - 2:
+            if 0 < ball[4] < 180:
+                ball[4] = randint(220, 320)
+            else:
+                ball[4] = randint(40, 140)
+            ball = move_ball(ball)
+            ball = move_ball(ball)
+            ball = move_ball(ball)
+        ball = move_ball(ball)
+            # xb, yb, r, color = move_ball(xb, yb, r, color, angle)
 pygame.quit()
